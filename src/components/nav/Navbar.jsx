7aae843e3d23574198d63d0d77f0618533077a2e
Jboy Abecia <jufel.abecia24@gmail.com>
2023-07-20 00:29:51 +0800

    tailwindcss refactor

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className=" bg-transparent text-black font-poppins sticky top-0">
+    <nav className="bg-transparent text-black font-poppins">
       <section className="container">
         <div className="h-[60px] flex justify-between items-center">
           <div className="text-[28px]">
@@ -10,7 +10,11 @@ const Navbar = () => {
             <span>store</span>
           </div>
           <div>Menu</div>
-          <div>Profile</div>
+          <div>
+            <button className="active:scale-75 text-white text-[14px] bg-orange-600 duration-500 rounded hover:bg-blue-400">
+              Login
+            </button>
+          </div>
         </div>
       </section>
     </nav>
